161e1aa3c325544e77ac579a55619e1663f1cfa3
Denys Koval <metalheadden@gmail.com>
2025-02-22 16:48:08 +0200

    slider-try

---
+++ src/js/swiper.js
@@ -0,0 +1,12 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+loop: true,
+
+  // If we need pagination
+pagination: {
+    el: '.swiper-pagination',
+},
+
+});
